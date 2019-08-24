c8b0e38d13eb809676209b10cad01f0a7d33f62f
Nikos Fasarakis Hilliard <nfasarakis@gmail.com>
2019-08-24 17:26:32 +0300

    Transforms Catalog item class component into functional component

--- app/components/CatalogItem/CatalogItem.js
+++ app/components/CatalogItem/CatalogItem.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {View, Text} from 'react-native';
 import styles from './style';
 
-export default class CatalogItem extends Component {
-
+export default function CatalogItem() {
   /**
    *
    * @return {[View]}
    */
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.titleContainer}>
-          <Text style={styles.titleText}> Title </Text>
-          <Text style={styles.price}> 5{'$'} </Text>
-        </View>
-        <Text style={styles.description}> Short description goes here </Text>
-        <View style={styles.horLine}></View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}> Title </Text>
+        <Text style={styles.price}> 5{'$'} </Text>
       </View>
-    );
-  }
+      <Text style={styles.description}> Short description goes here </Text>
+      <View style={styles.horLine} />
+    </View>
+  );
 }
--- app/components/CatalogItem/style.js
+++ app/components/CatalogItem/style.js
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
     borderBottomColor: '#CDCBCB',
     marginTop: 15,
     marginBottom: 15,
-  }
+  },
 });
 
 export default styles;
