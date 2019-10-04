47f1f45aed21aec21fb913da3ef77de70337dce9
Nikos Fasarakis Hilliard <nfasarakis@gmail.com>
2019-10-04 16:08:02 +0300

    Refactor IconButton into functional component

--- app/components/elements/IconButton/IconButton.js
+++ app/components/elements/IconButton/IconButton.js
@@ -1,37 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Image, TouchableOpacity} from 'react-native';
 import PropTypes from 'prop-types';
-//import styles from './style';
 
-
-export default class IconButton extends Component {
-
-  static propTypes = {
-    // Style for the TouchableOpacity component in render()
-    containerStyle: PropTypes.any.isRequired,
-    // Style for the Image component in render()
-    iconStyle: PropTypes.any.isRequired,
-    // Image component source property value for the Image component in render()
-    iconSource: PropTypes.any.isRequired,
-    // OnPress event handler for TouchableOpacity component in render()
-    onIconPress: PropTypes.func.isRequired,
-  }
-
+/**
+ * IconButton Component: Custom clickable image with callbacks. Can be used as image button.
+ *
+ * The IconButton Component receives the following props
+ *  @param {object} containerStyle Mandatory custom style for the TouchableOpacity
+ *                                 acting as the clicable icon
+ *  @param {object} iconStyle Mandatory custom style for the clickable image
+ *  @param {object} iconSource Source property value for the clickable image
+ *  @param {function} onIconPress OnPress event handler for the IconButton component
+ *
+ * @return {[View]} A styled TouchableOpacity component respresenting an image button
+ */
+export default function IconButton(props) {
   /**
-   * [Renders a touchable opacity component containing
-   * A) An Image component]
    * @return {[TouchableOpacity]} [TouchableOpacity component respresenting an icon button]
    */
-  render() {
-    return (
-      <TouchableOpacity
-        style={this.props.containerStyle}
-        onPress={this.props.onIconPress}>
-        <Image
-          style={this.props.iconStyle}
-          source={this.props.iconSource}
-        />
-      </TouchableOpacity>
-    );
-  }
+  return (
+    <TouchableOpacity style={props.containerStyle} onPress={props.onIconPress}>
+      <Image style={props.iconStyle} source={props.iconSource} />
+    </TouchableOpacity>
+  );
 }
+
+// PropTypes
+IconButton.propTypes = {
+  // Style for the TouchableOpacity component in render()
+  containerStyle: PropTypes.any.isRequired,
+  // Style for the Image component in render()
+  iconStyle: PropTypes.any.isRequired,
+  // Image component source property value for the Image component in render()
+  iconSource: PropTypes.any.isRequired,
+  // OnPress event handler for TouchableOpacity component in render()
+  onIconPress: PropTypes.func.isRequired,
+};
--- app/components/elements/IconButton/style.js
+++
@@ -1,7 +0,0 @@
-import {StyleSheet} from 'react-native';
-
-const styles = StyleSheet.create({
-  // No styles yet
-});
-
-export default styles;
