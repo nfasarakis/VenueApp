f510d7b352ea0abf75ecb943c13f1e3867872f21
Nikos Fasarakis Hilliard <nfasarakis@gmail.com>
2019-10-06 19:19:04 +0300

    Refactor TopTab into functional component

--- app/components/TopTab/TopTab.js
+++ app/components/TopTab/TopTab.js
@@ -1,44 +1,48 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {IconButton, SearchBar} from '../elements';
 import PropTypes from 'prop-types';
 import {SafeAreaView} from 'react-navigation';
 import styles from './style';
 
-export default class TopTab extends Component {
-  static propTypes = {
-    // CallBack function that fires on Press events for the filterIcon
-    onFilterIconPress: PropTypes.func.isRequired,
-    // CallBack function that fires on Press events for the toggling between
-    // list view and map view
-    onMapIconPress: PropTypes.func.isRequired,
-    // CallBack function that fires on focus events for the SearchBar
-    onSearchBarFocus: PropTypes.func.isRequired,
-  };
-
+/**
+ * TopTab Component: Component used as custom header in MainTabsNavigationOptions
+ *                   in /routerConfig.js
+ *                   Contains two clickable icons, a searchbar and corresp. callbacks
+ *
+ * The TopTab Component receives the following props
+ *  @param {function} onFilterIconPress CallBack function that fires on Press events for the filterIcon
+ *  @param {function} onMapIconPress CallBack function that fires on Press events for the mapIcon
+ *  @param {function} onSuggestionPress  CallBack function that fires when SearchBar is focused
+ *
+ * @return {<TouchableOpacity>} TouchableOpacity containg a suggested are name &
+ *                              number of venues contained within that area.
+ */
+export default function TopTab(props) {
   /**
-   * [Renders a Tab at the top of the Screen containing
-   * A) An IconButton component respresenting the filtering option
-   * B) A SearchBar component for performing searches on the venues
-   * C) An IconButton component respresenting the map-view option]
-   * @return {[View]} [View respresenting the TopTab component]
+   * @return {<View>} [View respresenting the TopTab component]
    */
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <IconButton
-          containerStyle={{}}
-          iconSource={require('../images/filter-icon.png')}
-          iconStyle={styles.filtericon}
-          onIconPress={this.props.onFilterIconPress}
-        />
-        <SearchBar onSearchBarFocus={this.props.onSearchBarFocus} />
-        <IconButton
-          containerStyle={{}}
-          iconSource={require('../images/map-icon.png')}
-          iconStyle={styles.mapicon}
-          onIconPress={this.props.onMapIconPress}
-        />
-      </SafeAreaView>
-    );
-  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <IconButton
+        containerStyle={{}}
+        iconSource={require('../images/filter-icon.png')}
+        iconStyle={styles.filtericon}
+        onIconPress={props.onFilterIconPress}
+      />
+      <SearchBar onSearchBarFocus={props.onSearchBarFocus} />
+      <IconButton
+        containerStyle={{}}
+        iconSource={require('../images/map-icon.png')}
+        iconStyle={styles.mapicon}
+        onIconPress={props.onMapIconPress}
+      />
+    </SafeAreaView>
+  );
 }
+
+//PropTypes
+TopTab.propTypes = {
+  onFilterIconPress: PropTypes.func.isRequired,
+  onMapIconPress: PropTypes.func.isRequired,
+  onSearchBarFocus: PropTypes.func.isRequired,
+};
